f22baf53d111d46a173791f7dbc34fd28ad19c8a
tehuanmelo <tehuanmelo@gmail.com>
2022-09-11 11:21:23 +0400

    blind-auction

---
+++ blind-auction/README.md
@@ -0,0 +1,8 @@
+# BLIND AUCTION 
+Is a type of auction, that all bidders simultaneously submit sealed bids so that no bidder knows the bid of any other participant. The highest bidder pays the price that was submitted
+## Installation
+Clone the folder into yor computer and rum the following command on the terminal
+***```bash
+python3 blind-auction.py
+```***
+
---
+++ blind-auction/blind-auction.py
@@ -0,0 +1,30 @@
+
+from art import logo
+
+bids = {}
+print(logo)
+print("Welcome to the secret auction program.")
+
+def find_highest(bids):
+  max_bid = 0
+  for bidder in bids:
+    if bids[bidder] > max_bid:
+      max_bid = bids[bidder]
+      
+  for bidder in bids:
+    if bids[bidder] == max_bid:
+      os.system('clear')
+      print(f'The winner is {bidder} with ${bids[bidder]} bid')
+
+more_bidders = True
+while more_bidders:
+  bidder_name = input("What is your name: ")
+  bidder_bid = int(input("What's your bid?: $"))
+  bids[bidder_name] = bidder_bid
+  flag = input("Are ther any other bid?: Yes or No ").lower()
+  if flag == "yes":
+    os.system('clear')
+  else:
+    more_bidders = False
+    find_highest(bids)
+    
